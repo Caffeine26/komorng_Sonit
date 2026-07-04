5c6af4fd1ed25de0db027bbd7ec603ce414e98e1
Sonit Sor <sorsonit06@gmail.com>
2026-07-04 20:49:02 +0700

    fix: make health controller public

--- backend/api/src/shared/health/health.controller.ts
+++ backend/api/src/shared/health/health.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../guards/public.decorator';
 
+@Public()
 @Controller('health')
 export class HealthController {
   @Get()
